1563dd7f8b52c40721f2e3b511c48c276d1f26c2
paco0161 <paco0161@gmail.com>
2023-10-18 16:56:02 -0400

    clean up unnecessary assets

--- assets/1.addJsonFileDummyKeyValue.ps1
+++
@@ -1,7 +0,0 @@
-$jsonfile = #path
-
-$json = Get-Content $jsonfile | Out-String | ConvertFrom-Json
-
-$json | Add-Member -Type NoteProperty -Name 'country' -Value 'Hong Kong'
-
-$json | ConvertTo-Json | Set-Content $jsonfile
