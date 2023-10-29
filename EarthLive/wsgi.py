b17a7db52e27ac854e9fa2a7bf3701a77fcd9449
paco0161 <paco0161@gmail.com>
2023-10-29 12:36:25 -0400

    Rename repository

--- EarthLive/wsgi.py
+++ EarthLive/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for WorldTimeClock_Python project.
+WSGI config for EarthLive project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WorldTimeClock_Python.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EarthLive.settings')
 
 application = get_wsgi_application()
--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import TimeZone
 
-# Create your tests here.
+class TimeZoneBaseTestCase(TestCase):
+    def testBase(self):
+        timeZone = TimeZone(continent="America", area="Toronto, Canada", time_zone="America/Toronto", abbreviation="GMT", utc_offset="+1:00", country="Canada")
+        self.assertEqual(timeZone.continent, "America")
+        self.assertEqual(timeZone.area, "Toronto, Canada")
+        self.assertEqual(timeZone.time_zone, "America/Toronto")
+        self.assertEqual(timeZone.abbreviation, "GMT")
+        self.assertEqual(timeZone.country, "Canada")
