e18e4f256fd121765f8559110800e365cf6042f1
paco0161 <paco0161@gmail.com>
2023-11-09 21:40:39 -0500

    prepare deploymemt to prod

--- EarthLive/settings/prod.py
+++ EarthLive/settings/prod.py
@@ -2,4 +2,14 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', ]
+ALLOWED_HOSTS = ["*"]
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+# SECURE_SSL_REDIRECT = True
+
+# SESSION_COOKIE_SECURE = True
+
+# CSRF_COOKIE_SECURE = True
+
+SECURE_HSTS_PRELOAD = True
