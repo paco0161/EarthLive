c13f6b28d48edc2844b4721af3fc78cb67ffb3e4
paco0161 <paco0161@gmail.com>
2023-11-11 13:34:46 -0500

    fix root_urlconf missing in vercel

--- EarthLive/settings/base.py
+++ EarthLive/settings/base.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from decouple import config
 
+ROOT_URLCONF = 'EarthLive.urls'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
