0dcd11bab22d1213ae052d0cbd6a7006e64ad799
paco0161 <paco0161@gmail.com>
2023-11-11 00:53:08 -0500

    deploy to vercel

--- EarthLive/settings/prod.py
+++ EarthLive/settings/prod.py
@@ -5,8 +5,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
-WSGI_APPLICATION = 'vercel_app.wsgi.app'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
