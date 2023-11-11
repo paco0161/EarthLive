6388274946683fc18dac4233ea0cc6acbd776892
paco0161 <paco0161@gmail.com>
2023-11-11 18:51:09 -0500

    add django settings module

--- EarthLive/settings/base.py
+++ EarthLive/settings/base.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from decouple import config
 
-ROOT_URLCONF = 'EarthLive.urls'
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,6 +123,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
+
+ROOT_URLCONF = 'EarthLive.urls'
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, "static"),
--- EarthLive/wsgi.py
+++ EarthLive/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EarthLive.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EarthLive.settings.base')
 
 application = get_wsgi_application()
 
