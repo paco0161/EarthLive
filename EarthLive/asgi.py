1496a8855bcc8c15441ef3c1efac9b814b717d0b
paco0161 <paco0161@gmail.com>
2023-11-11 19:18:01 -0500

    add settings in env variable

--- EarthLive/asgi.py
+++ EarthLive/asgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+from decouple import config
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EarthLive.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('SETTINGS_ENVIRONMENT'))
 
 application = get_asgi_application()
--- EarthLive/wsgi.py
+++ EarthLive/wsgi.py
@@ -8,10 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+from decouple import config
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EarthLive.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('SETTINGS_ENVIRONMENT'))
 
 application = get_wsgi_application()
 
